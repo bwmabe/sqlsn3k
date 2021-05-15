fb640ef5ba74bd12c2748697b0a01af2e85140d5
bwmabe <9590854+bwmabe@users.noreply.github.com>
2021-05-14 21:58:18 -0400

    initial version of 'test_sqlite_helpers.py'

---
+++ src/sqlsn3k/tests/test_sqlite_helpers.py
@@ -0,0 +1,23 @@
+import shutil
+import sqlite3
+
+from unittest import TestCase
+
+import sqlsn3k.sqlite_helpers
+
+
+class TestModifiesDB(TestCase):
+    def setUp(self):
+        """
+        Generates a list of queries that do or don't modify a database
+        """
+        self.queries = [("SELECT * FROM info ORDER BY lap_time ASC", False),
+                        ("SELECT * FROM info WHERE class='Military'", False),
+                        ("DROP TABLE t", True),
+                        ("CREATE TABLE t", True),
+                        ("INSERT INTO", True)]
+        self.modifies_db = sqlsn3k.sqlite_helpers.modifies_db
+
+    def test_modified(self):
+        for query in self.queries:
+            self.assertEqual(self.modifies_db(query[0]), query[1])
